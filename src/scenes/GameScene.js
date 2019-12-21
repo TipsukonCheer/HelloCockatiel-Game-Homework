d1f05a7cb9bb2699c9ee4f82709e134537d40fa3
Thanapat <lengleng9090@gmail.com>
2019-12-21 17:06:43 +0700

    [update]
    bugger

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,13 +3,14 @@ import Player from './Core/Player'
 import Object from './Core/Object'
 import Score from './core/Score'
  import Monster from './Core/Monster' 
-let bg
+let bg;
 let player;
-let ground_1, ground_2, groundS_1, groundS_2, bg
-let object,coin
+let ground_1;
+let object
+let coin;
 let score;
-let monster
-let sth
+let monster;
+let sth;
 class GameScene extends Phaser.Scene {
     constructor(config) {
         super({
@@ -20,15 +21,15 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.spritesheet('player', './src/image/character.png', { frameWidth: 416, frameHeight: 454 })
         this.load.image('Monster','src/image/flyMan_stand.png' , {frameWidth :122 , frameHeight:139}) 
-        bg.preload();
         bg = new BG({scene: this});
-        object.preload();
+        bg.preload();
         object = new Object({scene: this});
+        object.preload();
     }
     create() {
+        bg.create();
         player = new Player({ scene: this});
         player.create();
-        bg.create();
        object.create();
         score = new Score({ scene: this})
         score.create();
