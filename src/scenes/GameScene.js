9d2f94113f5811895ee9cb99c66fd6cfc2a11a30
Thanapat <lengleng9090@gmail.com>
2019-12-21 16:18:52 +0700

    [ADD] Player 100%

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,20 +1,24 @@
+import Player from './Core/Player'
+let player;
 class GameScene extends Phaser.Scene {
-    constructor(test) {
+    constructor(config) {
         super({
             key: 'GameScene'
         });
     }
 
     preload() {
-
+        this.load.spritesheet('player', './src/image/character.png', { frameWidth: 416, frameHeight: 454 })
     }
 
     create() {
+        player = new Player({ scene: this});
+        player.create();
 
     }
 
     update() {
-   
+       player.update();
     }
 }
 
