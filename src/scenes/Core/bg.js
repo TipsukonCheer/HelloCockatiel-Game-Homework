c32a2a65e87bd08b2afa583d17428bed69696917
pattarapol <pattarapol.plasma@mail.kmutt.ac.th>
2019-12-21 16:46:04 +0700

    [UPDATE] Background 100%

--- src/scenes/Core/bg.js
+++ src/scenes/Core/bg.js
@@ -1,4 +1,7 @@
 let phasers;
+let bg;
+let bg_cloud
+let mountain
 
 import 'phaser' ;
 
@@ -11,15 +14,19 @@ class Gamescene extends Phaser.Scene {
     }
 
     preload(){
-        
+        phasers.load.image('bg','src/image/sky_lightened.png')
+        phasers.load.image('bg_cloud', 'src/image/clouds_BG.png')
+        phasers.load.image('mountain', 'src/image/mountains_lightened.png')
     }
 
     create(){
-
+        bg = phasers.add.tileSprite(0, 0, 384, 216, 'bg').setOrigin(0, 0).setSize(800, 600).setScale(2.8)
+        bg_cloud = phasers.add.tileSprite(0, 0, 384, 216, 'bg_cloud').setOrigin(0, 0).setSize(800, 600).setScale(2.8)
+        mountain = phasers.add.image(400,300 , 'mountain').setScale(2.8)
     }
 
     update(){
-
+        bg_cloud.tilePositionX += 0.5
     }
 
 }
